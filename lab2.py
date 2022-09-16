d59ca47bf915a1dbd87e7c12b4a02c7f1d23c63b
DaniilPonomarev-Web <84072104+DaniilPonomarev-Web@users.noreply.github.com>
2022-09-16 19:30:59 +0500

    Initial commit

---
+++ lab2.py
@@ -0,0 +1,20 @@
+
+var_1 = 1;
+var_2 = 2;
+
+if var_1 != var_2 and var_1 == 1:  print("TRUE (var_1 не равен var_2, а var_1 равен 1)")
+else:  print('FALSE (var_1 равен var_2, а var_1 не равен 1)');
+
+
+if var_1*var_2 != 2 or var_2*1 != 2: print("TRUE")
+else: print("false");
+
+if var_1/var_2 == 0.5 and var_2/var_1 == 1: print('true')
+else: print('false')
+
+if  str(var_1) != str(var_2) or var_2*0.5 != var_1: print ('true')
+else: print ('false')
+	
+if var_1 == var_2: print('12321321321')
+else: print('gfdsgfsgdfsg')
+	
